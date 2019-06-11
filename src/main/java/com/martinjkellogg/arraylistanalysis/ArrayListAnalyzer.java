29fc783263f9f6661573e84899c524e3cb930d40
Martin Kellogg <kelloggm@cs.washington.edu>
2019-06-11 11:44:13 -0700

    like, all the code

--- src/main/java/com/martinjkellogg/arraylistanalysis/ArrayListAnalyzer.java
+++ src/main/java/com/martinjkellogg/arraylistanalysis/ArrayListAnalyzer.java
@@ -10,6 +10,7 @@ import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.IntegerLiteralExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.stmt.BlockStmt;
@@ -20,14 +21,21 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
+import com.github.javaparser.resolution.types.ResolvedReferenceType;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.utils.CodeGenerationUtils;
 import com.github.javaparser.utils.SourceRoot;
-import sun.jvm.hotspot.opto.Block;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -37,10 +45,20 @@ import java.util.stream.Collectors;
 public class ArrayListAnalyzer {
 
     public static void main(String[] args) throws IOException {
+
+        // Set up a minimal type solver that only looks at the classes used to run this sample.
+        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
+        combinedTypeSolver.add(new ReflectionTypeSolver());
+
+        // Configure JavaParser to use type resolution
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
+
         // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
         // In this case the root directory is found by taking the root from the current Maven module,
         // with src/main/resources appended.
         SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot(ArrayListAnalyzer.class).resolve("src/main/resources"));
+
+        sourceRoot.getParserConfiguration().setSymbolResolver(symbolSolver);
 
         // Our sample is in the root of this directory, so no package name.
         CompilationUnit cu = sourceRoot.parse("", "SimpleYesTransform.java");
@@ -52,52 +70,19 @@ public class ArrayListAnalyzer {
                         n.getChildNodes().stream().filter(n1 -> n1 instanceof ForEachStmt).collect(Collectors.toList());
 
                 for (Node node : forEachNodes) {
-
-                    // the level of evil here is very high, but this totally works lol
-                    // the default String representation of a UUID separates the different parts with dashes. That
-                    // violates the Java identifier rules, so get rid of those.
-                    // gotta start with "lv" to avoid starting an identifier with a number, also.
-                    final String loopvarName = "lv" + UUID.randomUUID().toString().replaceAll("-", "");
-
-                    ForEachStmt forEachStmt = (ForEachStmt) node;
-                    String listName = forEachStmt.getIterable().asNameExpr().getNameAsString();
-                    VariableDeclarationExpr varDecl = forEachStmt.getVariable().clone();
-
-                    ForStmt noIteratorStmt = new ForStmt();
-
-                    VariableDeclarationExpr declarator = new VariableDeclarationExpr(PrimitiveType.intType(), loopvarName);
-                    IntegerLiteralExpr literallyZero = new IntegerLiteralExpr(0);
-                    AssignExpr assignExpr = new AssignExpr(declarator, literallyZero, AssignExpr.Operator.ASSIGN);
-                    NodeList<Expression> initalizer = new NodeList<>(assignExpr);
-                    noIteratorStmt.setInitialization(initalizer);
-
-                    BinaryExpr comparison = new BinaryExpr(new NameExpr(loopvarName),
-                            new MethodCallExpr(new NameExpr(listName), new SimpleName("size")),
-                            BinaryExpr.Operator.LESS);
-
-                    noIteratorStmt.setCompare(comparison);
-
-                    AssignExpr updateExpr = new AssignExpr(new NameExpr(loopvarName),
-                            new IntegerLiteralExpr(1), AssignExpr.Operator.PLUS);
-                    NodeList<Expression> update = new NodeList<>(updateExpr);
-                    noIteratorStmt.setUpdate(update);
-
-                    Statement body = forEachStmt.getBody().clone();
-                    NodeList<Expression> args = new NodeList<>(new NameExpr(loopvarName));
-                    AssignExpr assignExpr1 = new AssignExpr(varDecl,
-                            new MethodCallExpr(new NameExpr(listName), new SimpleName("get"), args),
-                            AssignExpr.Operator.ASSIGN);
-
-                    BlockStmt newBody = new BlockStmt();
-                    newBody.addStatement(assignExpr1);
-
-                    for (Statement statement : body.asBlockStmt().getStatements()) {
-                        newBody.addStatement(statement);
+                    // Need to do a series of checks to prove that the replacement is safe:
+                    // 1. the thing being looped over must be an array list
+                    // 2. the array list must be locally-scoped
+                    // 3. the value of the array list must *only* come from a call to the array list constructor
+                    if (!isLocalArrayList(node, n)) {
+                        continue;
                     }
 
-                    noIteratorStmt.setBody(newBody);
+                    if (callsModificationMethods(node)) {
+                        continue;
+                    }
 
-                    n.replace(forEachStmt, noIteratorStmt);
+                    doReplace(n, node);
                 }
 
                 return super.visit(n, arg);
@@ -110,5 +95,177 @@ public class ArrayListAnalyzer {
                 CodeGenerationUtils.mavenModuleRoot(ArrayListAnalyzer.class)
                         // appended with a path to "output"
                         .resolve(Paths.get("output")));
+    }
+
+    private static boolean isLocalArrayList(Node node, BlockStmt n) {
+        ForEachStmt forEachNode = (ForEachStmt) node;
+
+        Expression iterable = forEachNode.getIterable();
+
+        if (!iterable.isNameExpr()) {
+            return false;
+        }
+
+        NameExpr iterableName = iterable.asNameExpr();
+
+        ResolvedType resolvedType = iterable.calculateResolvedType();
+
+        if (!resolvedType.isReferenceType()) {
+            return false;
+        }
+
+        ResolvedReferenceType refType = resolvedType.asReferenceType();
+        if (!("java.util.ArrayList".equals(refType.getQualifiedName()) ||
+                "java.util.List".equals(refType.getQualifiedName()))) {
+            return false;
+        }
+
+
+        List<AssignExpr> allAssignments = n.findAll(AssignExpr.class);
+        List<VariableDeclarationExpr> allDecls = n.findAll(VariableDeclarationExpr.class);
+        
+        // if there were no declarations in the method body, then the foreach loop can't be over a local variable
+        if (allDecls.size() == 0) {
+            return false;
+        }
+
+        boolean wasDeclaredLocally = false;
+        // if it wasn't declared locally, reject
+        for (VariableDeclarationExpr declExpr : allDecls) {
+            for (VariableDeclarator declarator : declExpr.getVariables()) {
+                if (declarator.getName().equals(iterableName.getName())) {
+                    if (declarator.getInitializer().isPresent() &&
+                            declarator.getInitializer().get().isObjectCreationExpr()) {
+
+                        ObjectCreationExpr declLhs = declarator.getInitializer().get().asObjectCreationExpr();
+
+                        if ("ArrayList".equals(declLhs.getType().getName().toString())) {
+                            wasDeclaredLocally = true;
+                        }
+                    }
+                } else if (declarator.getInitializer().isPresent() &&
+                    declarator.getInitializer().get().isNameExpr()) {
+                    // if a variable is assigned with the value of the iterable, reject the code
+                    // because its being aliased
+                    NameExpr nameExprLhs = declarator.getInitializer().get().asNameExpr();
+                    if (nameExprLhs.getName().equals(iterableName.getName())) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (!wasDeclaredLocally) {
+            return false;
+        }
+
+        // Check for aliases
+        // Note that I have to use a foreach loop instead of a lambda here b/c I need to be able to return
+        for (AssignExpr assignExpr : allAssignments) {
+            Expression rhs = assignExpr.getTarget();
+
+            // if the right hand side is a name expression containing the variable of interest, reject.
+            // We're only looking for effectively final locals
+            if (rhs.isNameExpr()) {
+                NameExpr rhsAsName = rhs.asNameExpr();
+                if (rhsAsName.getName().equals(iterableName.getName())) {
+                    return false;
+                }
+            }
+
+            Expression lhs = assignExpr.getValue();
+            // if the variable of interest ever appears on the lhs of an assignment, reject. Possibly alias.
+            if (lhs.isNameExpr()) {
+                NameExpr lhsAsName = lhs.asNameExpr();
+                if (lhsAsName.getName().equals(iterableName.getName())) {
+                    return false;
+                }
+            }
+        }
+
+        // if a parameter to a method call is the list anywhere, reject because it might alias it.
+        List<MethodCallExpr> methodCalls = n.findAll(MethodCallExpr.class);
+        for (MethodCallExpr methodCallExpr : methodCalls) {
+            for(Expression argument : methodCallExpr.getArguments()) {
+                if (argument.isNameExpr()) {
+                    NameExpr argAsName = argument.asNameExpr();
+                    if (argAsName.getName().equals(iterableName.getName())) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static final String[] bannedMethods = {
+            "add", "addAll", "remove", "removeAll", "removeIf", "removeRange", "retainAll"};
+    private static final Set<String> bannedMethodsSet = new HashSet<String>();
+    static {
+        bannedMethodsSet.addAll(Arrays.asList(bannedMethods));
+    }
+
+    private static boolean callsModificationMethods(Node stmt) {
+        ForEachStmt forEachStmt = (ForEachStmt) stmt;
+
+        for (MethodCallExpr methodCall : forEachStmt.findAll(MethodCallExpr.class)) {
+            if (bannedMethodsSet.contains(methodCall.getName().toString())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void doReplace(BlockStmt n, Node node) {
+
+        // the level of evil here is very high, but this totally works lol
+        // the default String representation of a UUID separates the different parts with dashes. That
+        // violates the Java identifier rules, so get rid of those.
+        // gotta start with "lv" to avoid starting an identifier with a number, also.
+        final String loopvarName = "lv" + UUID.randomUUID().toString().replaceAll("-", "");
+
+        ForEachStmt forEachStmt = (ForEachStmt) node;
+        String listName = forEachStmt.getIterable().asNameExpr().getNameAsString();
+        VariableDeclarationExpr varDecl = forEachStmt.getVariable().clone();
+
+        ForStmt noIteratorStmt = new ForStmt();
+
+        VariableDeclarationExpr declarator = new VariableDeclarationExpr(PrimitiveType.intType(), loopvarName);
+        IntegerLiteralExpr literallyZero = new IntegerLiteralExpr(0);
+        AssignExpr assignExpr = new AssignExpr(declarator, literallyZero, AssignExpr.Operator.ASSIGN);
+        NodeList<Expression> initalizer = new NodeList<>(assignExpr);
+        noIteratorStmt.setInitialization(initalizer);
+
+        BinaryExpr comparison = new BinaryExpr(new NameExpr(loopvarName),
+                new MethodCallExpr(new NameExpr(listName), new SimpleName("size")),
+                BinaryExpr.Operator.LESS);
+
+        noIteratorStmt.setCompare(comparison);
+
+        AssignExpr updateExpr = new AssignExpr(new NameExpr(loopvarName),
+                new IntegerLiteralExpr(1), AssignExpr.Operator.PLUS);
+        NodeList<Expression> update = new NodeList<>(updateExpr);
+        noIteratorStmt.setUpdate(update);
+
+        Statement body = forEachStmt.getBody().clone();
+        NodeList<Expression> args = new NodeList<>(new NameExpr(loopvarName));
+        AssignExpr assignExpr1 = new AssignExpr(varDecl,
+                new MethodCallExpr(new NameExpr(listName), new SimpleName("get"), args),
+                AssignExpr.Operator.ASSIGN);
+
+        BlockStmt newBody = new BlockStmt();
+        newBody.addStatement(assignExpr1);
+
+        for (Statement statement : body.asBlockStmt().getStatements()) {
+            newBody.addStatement(statement);
+        }
+
+        noIteratorStmt.setBody(newBody);
+
+        noIteratorStmt.setComment(forEachStmt.getComment().isPresent() ? forEachStmt.getComment().get() : null);
+
+        n.replace(forEachStmt, noIteratorStmt);
     }
 }
--- src/main/resources/SimpleYesTransform.java
+++ src/main/resources/SimpleYesTransform.java
@@ -3,7 +3,7 @@ import java.util.*;
 class SimpleYesTransform {
 
     public void doStuff() {
-        List<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("hello");
         list.add(" ");
         list.add("world");
@@ -11,9 +11,217 @@ class SimpleYesTransform {
 
         list.size();
 
+        // should be transformed
         for (String s : list) {
             System.out.print(s);
         }
+    }
+
+    public void callsSize() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should be transformed
+        for (String s : list) {
+            list.size();
+        }
+    }
+
+    public void doStuffNotAnArrayList() {
+        List<String> list = new LinkedList<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void doStuffNotAList() {
+        Set<String> list = new HashSet<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void doStuffWithValueFromOtherPlace1() {
+        List<String> list = getList();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void doStuffWithValueFromOtherPlace2(List<String> list) {
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void reassignList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+        list = new LinkedList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void aliasList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        List<String> alias = list;
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void aliasList2() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        List<String> alias;
+        alias = list;
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void aliasListViaCall() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add(" ");
+        list.add("world");
+        list.add("!");
+
+        list.size();
+
+        mightAliasList(list);
+
+        // should not be transformed
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+
+    public void callsAdd() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.add(null);
+        }
+    }
+
+    public void callsAddAll() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.addAll(Collections.EMPTY_LIST);
+        }
+    }
+
+    public void callsRemove() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.remove(null);
+        }
+    }
+
+    public void callsRemoveAll() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.removeAll(Collections.EMPTY_LIST);
+        }
+    }
+
+    public void callsRemoveIf() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.removeIf(b -> false);
+        }
+    }
+
+    public void callsRemoveRange() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.removeRange(0, 0);
+        }
+    }
+
+    public void callsRetainAll() {
+        ArrayList<String> list = new ArrayList<>();
+
+        // should not be transformed
+        for (String s : list) {
+            list.retainAll(Collections.EMPTY_LIST);
+        }
+    }
+
+    private List getList() {
+        return new LinkedList();
+    }
+
+    private void mightAliasList(List list) {
+
     }
 
 }
